daf5f3e9b6c6e32ccc6a638610e92035c745d76d
guessi <guessi@gmail.com>
2025-08-24 01:08:12 +0800

    chore(misc): launch only necessary components by default

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,29 +3,29 @@
 echo "[debug] starting bbsd ..."
 /home/bbs/bin/bbsd
 
-echo "[debug] starting bmtad ..."
-/home/bbs/bin/bmtad
-
-echo "[debug] starting gemd ..."
-/home/bbs/bin/gemd
-
-echo "[debug] starting bhttpd ..."
-/home/bbs/bin/bhttpd
-
-echo "[debug] starting bguard ..."
-/home/bbs/bin/bguard
-
-echo "[debug] starting bpop3d ..."
-/home/bbs/bin/bpop3d
-
-echo "[debug] starting bnntpd ..."
-/home/bbs/bin/bnntpd
-
-echo "[debug] starting xchatd ..."
-/home/bbs/bin/xchatd
-
-echo "[debug] starting innbbsd ..."
-/home/bbs/innd/innbbsd
+# echo "[debug] starting bmtad ..."
+# /home/bbs/bin/bmtad
+#
+# echo "[debug] starting gemd ..."
+# /home/bbs/bin/gemd
+#
+# echo "[debug] starting bhttpd ..."
+# /home/bbs/bin/bhttpd
+#
+# echo "[debug] starting bguard ..."
+# /home/bbs/bin/bguard
+#
+# echo "[debug] starting bpop3d ..."
+# /home/bbs/bin/bpop3d
+#
+# echo "[debug] starting bnntpd ..."
+# /home/bbs/bin/bnntpd
+#
+# echo "[debug] starting xchatd ..."
+# /home/bbs/bin/xchatd
+#
+# echo "[debug] starting innbbsd ..."
+# /home/bbs/innd/innbbsd
 
 echo "[debug] starting camera ..."
 su bbs -c '/home/bbs/bin/camera'
