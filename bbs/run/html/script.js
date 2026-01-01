6752868281cf2c9a81a4a2cb2e774f299539f447
guessi <guessi@gmail.com>
2026-01-01 20:53:12 +0800

    feat(js): code cleanup and optimization
    - Remove deprecated #share handler (removed by 0f3fe567)
    - Update key mapping comments for clarity
    - Minor code optimization improvements
    - Extract magic numbers for better maintainability

--- bbs/run/html/script.js
+++ bbs/run/html/script.js
@@ -1,29 +1,30 @@
-var curr = 0;
-var keyEvent = 0;
-var baseurl = "http://bbs.ndhu.edu.tw:8080";
+let curr = 0;
+let keyEvent = 0;
+const baseurl = "http://bbs.ndhu.edu.tw:8080";
+const maxItemsPerPage = 20;
+const maxPosition = maxItemsPerPage - 1; // 19
 
 $.ajaxSetup({
-  cache : false
+  cache: false
 });
 
 const checkURL = (p) => {
   /* 0: not valid, 1: valid */
-  return (typeof p != "undefined" && p.length > 0);
+  return p && p.length > 0;
 }
 
 const highlight = (pos) => {
-  if (pos >= 0 && pos <= 19) {
+  if (pos >= 0 && pos <= maxPosition) {
     $("#curPos").val(pos);
-    $(".listitem:not(:nth-child(" + (pos + 1) + "))").removeClass('highlight');
-    $(".listitem:nth-child(" + (pos + 1) + ")").addClass("highlight");
+    $(".listitem").removeClass('highlight').eq(pos).addClass("highlight");
   }
 }
 
 const loadPage = (next, pos) => {
-  if (pos >= 0 && pos <= 19) {
+  if (pos >= 0 && pos <= maxPosition) {
     $("#content").load(next + " #content", () => {
-      var loc = (next.charAt(0) == '?') ? (window.location.pathname + next) :
-                (next.charAt(0) == '/') ? (baseurl + next) : "";
+      const loc = (next.charAt(0) == '?') ? (window.location.pathname + next) :
+                  (next.charAt(0) == '/') ? (baseurl + next) : "";
       if (checkURL(loc)) {
         window.history.pushState("", document.title, loc);
       }
@@ -33,9 +34,9 @@ const loadPage = (next, pos) => {
 }
 
 const keyEventHandler = (key) => {
-  var link = "";
+  let link = "";
   /* 1 ~ 20 */
-  var ilen = $(".listitem").length;
+  const ilen = $(".listitem").length;
   /* 0 ~ 19 */
   curr = parseInt($("#curPos").val());
   
@@ -46,7 +47,7 @@ const keyEventHandler = (key) => {
     /* class, [C:67], [c:99] */
     window.location.href = "/class";
   } else if (key == 72 || key == 104) {
-    /* hotboard, [M:72], [m:104] */
+    /* hotboard, [H:72], [h:104] */
     loadPage("/hotboard", 1);
   } else if (key == 77 || key == 109) {
     /* home, [M:77], [m:109] */
@@ -126,7 +127,7 @@ const keyEventHandler = (key) => {
       if (!checkURL(link)) {
         return;
       }
-      curr = 19;
+      curr = maxPosition;
       $("#curPos").val(curr);
       loadPage(link, curr);
     }
@@ -154,25 +155,22 @@ $(() => {
   /* A(65)~Z(90), a(97)~z(122) */
   $(document).keypress((e) => {
     /* ctrl/alt/shift whould never happend at keyPress */
-    var key = e.which ? e.which : (e.keyCode ? e.keyCode : 0);
+    const key = e.which || e.keyCode || 0;
     if (!keyEvent) {
       keyEventHandler(key);
     }
   });
   
   $(document).keydown((e) => {
-    var ctrlKey = e.ctrlKey;
-    var shiftKey = e.shiftKey;
-    var altKey = e.altKey;
-    if (ctrlKey || shiftKey || altKey) {
-      return
-    };
+    if (e.ctrlKey || e.shiftKey || e.altKey) {
+      return;
+    }
     keyEvent = 1;
-    var key = e.which ? e.which : (e.keyCode ? e.keyCode : 0);
+    const key = e.which || e.keyCode || 0;
     keyEventHandler(key);
   });
   
-  $(document).keyup((e) => {
+  $(document).keyup(() => {
     /* restore */
     keyEvent = 0;
   });
@@ -189,36 +187,27 @@ $(() => {
   $("#plink span input").click((e) => {
     $(e.currentTarget).focus().select();
   });
-  
-  $("#share").click((e) => {
-    if ($("#share .hidden").css("display") == "none") {
-      $("#share .hidden").css("display", "block");
-    } else {
-      $("#share .hidden").css("display", "none");
-    }
-  });
 });
 
 /* Disappearing "Scroll to top" link with jQuery and CSS http://briancray.com/2009/10/06/scroll-to-top-link-jquery-css/ */
 $(() => {
-  var timer;
-  var show = false;
-  var $box = $('#message a');
-  var $window = $(window);
-  var top = $(document.body).children().first().position().top;
+  let timer, show = false;
+  const $box = $('#message a');
+  const $window = $(window);
+  const top = $(document.body).children().first().position().top;
   
   $window.scroll(() => {
-    window.clearTimeout(timer);
-    timer = window.setTimeout(() => {
-        if ($window.scrollTop() <= top) {
-          show = false;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      if ($window.scrollTop() <= top) {
+        show = false;
+        $box.fadeOut(500);
+      } else if (show == false) {
+        show = true;
+        $box.stop(true, true).show().click(() => {
           $box.fadeOut(500);
-        } else if (show == false) {
-          show = true;
-          $box.stop(true, true).show().click(() => {
-            $box.fadeOut(500);
-          });
-        }
-      }, 100);
+        });
+      }
+    }, 100);
   });
 });
